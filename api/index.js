fb2b6e5e09515f4b76c31bfd2db8339761b349c1
Abel Hinostroza <hinostrozajavier5b@gmial.com>
2025-02-05 21:27:27 -0500

    Deploy in Versel 13

--- api/index.js
+++ api/index.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// const whitelist = ['http://localhost:8080', 'https://myapp.co', 'https://FDeploymentHA.onrender.com', 'https://f-deployment-ha.vercel.app'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'https://FDeploymentHA.onrender.com', 'https://f-deployment-ha.vercel.app'];
+
 const options = {
-  origin: '*'
-  /* (origin, callback) => {
+  origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('No permitido'));
     }
-  } */
+  }
 };
 app.use(cors(options));
 
